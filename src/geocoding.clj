0450c9dfb628f1b2b890ac8cfd0d700f6610288b
Erik Strömberg <erik.stromberg@gmail.com>
2016-03-31 15:00:50 +0200

    Changes

--- src/geocoding.clj
+++ src/geocoding.clj
@@ -4,18 +4,28 @@
             [camel-snake-kebab.extras :refer [transform-keys]]
             [camel-snake-kebab.core :refer :all]))
 
-(def api-key "AIzaSyDowYmjIt7u1eA0VD375Mxa1iS0cl1SkPM")
-
 (def base-url "https://maps.googleapis.com/maps/api/geocode/")
 
 (defn keywordize-keys [m]
   (let [f (fn [[k v]] (if (string? k) [(->kebab-case-keyword k) v] [k v]))]
     (clojure.walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
 
+(defn get-address-components [m]
+  (reduce (fn [m' ac]
+            (assoc m'
+                   (->kebab-case-keyword (get-in ac [:types 0]))
+                   (dissoc ac :types)))
+          {}
+          m))
+
+(defn format-result [{:keys [address-components] :as m}]
+  (dissoc (merge m (get-address-components address-components)) :address-components))
+
 (defn geocode [s opts]
-  (-> (http/get (str base-url "json")
-                {:query-params (assoc (transform-keys ->snake_case opts) :address s)})
-      :body
-      json/read-str
-      keywordize-keys
-      :results))
+  (->> (http/get (str base-url "json")
+                 {:query-params (assoc (transform-keys ->snake_case opts) :address s)})
+       :body
+       json/read-str
+       keywordize-keys
+       :results
+       (map format-result)))
